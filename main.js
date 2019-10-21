b85478174c9f0c5ef7e22d0bfc6d22e37afb9ba4
webdenis <salad40@gmail.com>
2019-10-21 22:59:27 +0300

    generate objects from middle, color modes and rotation functionality

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 $(function() {
 	
-	// Interval
 	var interval = 1000;
 	var color = 0;
+	var rectAnimation = 'none';
 	
+	/* 
+		1 - grayscale
+		2 - random non grayscale
+		3 - black to white to black
+	*/
+	var currentColorMode = 1;
+	
+	// Mousemove interactivity
 	$(document).mousemove(function(getCurrentPos){
 		var xCord = getCurrentPos.clientX;
 		var yCord = getCurrentPos.clientY;
@@ -11,30 +19,105 @@ $(function() {
 		var xPercent = Math.floor(xCord / $(window).width() * 100);
 		var yPercent = Math.floor(yCord / $(window).height() * 100);
 		
-		//console.log(xPercent, yPercent);
-		interval = xPercent * 5;
-		color = (Math.random() < 0.5 ? -1 : 1) * yPercent * 2.55;
+		// Bottom to top = box generation interval
+		interval = yPercent * 5 - 300;
+		
+		// Left to right = left or right rotation speed
+		let speed;
+		if (xPercent < 49) {
+			speed = 0.05 + (xPercent / 5);
+			rectAnimation = 'rotatingLeft '+speed.toFixed(1)+'s linear infinite';
+		} else if (xPercent > 51) {
+			speed = 5.05 - (xPercent - 50) / 10;
+			rectAnimation = 'rotatingRight '+speed.toFixed(1)+'s linear infinite';
+		} else {
+			rectAnimation = 'none';
+		}
 		
 		
 	});
 	
-	function randColor() {
-		//return '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-		//return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-		
-
-		/*var value = Math.random() * 0xFF | 0;
+	// Color functions
+	function getCurrentColor() {
+		let x = 1;
+		switch(currentColorMode) {
+			case 1:
+				x = getGrayscale();
+				break;
+			case 2:
+				x = getRandomColor();
+				break;
+			case 3:
+				x = getGrayscalePeriodic();
+				break;
+			case 4:
+				x = getRandomBackground();
+				break;
+			default:
+				x = getGrayscale();
+		}
+		return x;
+	}
+	
+	function getGrayscale() { // 1
+		var value = Math.random() * 0xFF | 0;
 		var grayscale = (value << 16) | (value << 8) | value;
 		var color = '#' + grayscale.toString(16);
-		return color;*/
-		
-		return ("url('pic"+(Math.random() < 0.5 ? 1 : 2)+".jpg') "+color+"% "+color+"%");
-
-		//return 'rgb('+color+','+color+','+color+')';
+		return color;
 	}
-	/*var counter = 0;
-	var up = true;*/
 	
+	function getRandomColor() { // 2
+		return '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+	}
+	
+	var counter = 0;
+	var up = true;
+	function getGrayscalePeriodic() { // 3
+		if (up) {
+			counter += 20;
+		} else {
+			counter -= 20;
+		}
+		
+		if (counter === 260) {
+			up = false;
+		}
+		
+		if (counter === 0) {
+			up = true;
+		}
+		
+		return 'rgb('+counter+','+counter+','+counter+')';
+	}
+	
+	function getRandomBackground() {
+		let a = Math.floor(Math.random() * 100) + 1;
+		let b = Math.floor(Math.random() * 100) + 1;
+		return ("url('pic"+(Math.random() < 0.5 ? 1 : 2)+".jpg') "+a+"% "+b+"%");
+	}
+	// End of color functions
+	
+	// Color switch
+	var firstColor = 1;
+	var lastColor = 4;
+	
+	$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+		currentColorMode--;
+		if (currentColorMode === (firstColor - 1)) { currentColorMode = lastColor; }
+        break;
+
+        case 39: // right
+		currentColorMode++;
+		if (currentColorMode === (lastColor + 1)) { currentColorMode = firstColor; }
+        break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
+
 	// Prepare parameters and call le job
 	$.fn.calcAndGo = function() {
 		let x, y; // 1 = +, -1 = -
@@ -62,7 +145,7 @@ $(function() {
 		$(this).letsGo(x, y);
 	};
 	
-	// Move current, create new one underneath
+	// Move current and create new one
 	$.fn.letsGo = function(x, y) {
 	  
 		let rect = $(this[0]);
@@ -71,22 +154,25 @@ $(function() {
 		let prepy = rect.offset().top + y * rect.height();
 		let prepx = rect.offset().left + x * rect.width();
 
-		rect.css('left', prepx);
-		rect.css('top', prepy);
+		rect.css('left', '-25%');
+		rect.css('top', '-25%');
+		rect.css('width', '150%');
+		rect.css('height', '150%');
 		
 		let cns = this[0].className;
 		
-		let randomColor = randColor();
+		let randomColor = getCurrentColor();
 		let newRect = $('<div />', {
 			"class": cns
 		})
 		.css({
 			'background' : randomColor,
 		})
+		.css('-webkit-animation', rectAnimation)
 		/*.mouseover(function() {
 			$(this).calcAndGo();
 		})*/
-		.prependTo('.main');
+		.appendTo('.main');
 		
 		rect.addClass('setForRemoval');
 		setTimeout(function() {
@@ -110,7 +196,7 @@ $(function() {
 	
 	function go() {
 		setTimeout(function() {
-			$(".rect").each(function() {
+			$(".fullrect").each(function() {
 				if (! $(this).hasClass('setForRemoval')) {
 					$(this).calcAndGo();
 				}
